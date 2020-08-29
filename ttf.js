9d181f02557036db53f9cc32e70cb2a45c7f82f6
Bokwang0310 <chdl2353@naver.com>
2020-08-29 23:41:39 +0900

    change some dir name and var name

--- ttf.js
+++ ttf.js
@@ -2,11 +2,11 @@ const svg2ttf = require("svg2ttf");
 const ImageTracer = require("./public/javascripts/imagetracer_v1.2.1");
 const PNG = require("pngjs").PNG;
 const fs = require("graceful-fs"); // EMFILE 에러 해결을 위한 fs 라이브러리
+const SVGIcons2SVGFontStream = require("svgicons2svgfont");
 
-module.exports = (customOption, save_dir) => {
-  const SVGIcons2SVGFontStream = require("svgicons2svgfont");
+module.exports = (customOption, save_dir, fontName) => {
   let fontStream = new SVGIcons2SVGFontStream({
-    fontName: "customfont",
+    fontName: fontName,
   });
 
   // 폴더 에러 방지
@@ -14,7 +14,7 @@ module.exports = (customOption, save_dir) => {
     fs.statSync("svg_font");
   } catch (err) {
     if (err.code === "ENOENT") {
-      fs.mkdirSync("svg-font");
+      fs.mkdirSync("svg_font");
     }
   }
 
@@ -34,27 +34,25 @@ module.exports = (customOption, save_dir) => {
     }
   }
 
-  let files = fs.readdirSync(`${save_dir}`);
+  let fileNames = fs.readdirSync(save_dir);
 
   let sources = [];
-  let fileName = [];
 
   // 원본 파일 이름으로부터 유니코드명 및 svg 파일 이름 추출
   for (let i = 0; i < files.length; i++) {
-    sources[i] = "0x" + files[i].substring(0, 5);
-    fileName[i] = files[i].substring(0, 5);
+    sources[i] = "0x" + fileNames[i].substring(0, 5);
   }
 
   // 각각의 이미지로부터 각각의 SVG 파일 생성
   for (let i = 0; i < files.length; i++) {
     let j = i;
 
-    let data = fs.readFileSync(`${save_dir}/${fileName[j]}.png`);
+    let data = fs.readFileSync(`${save_dir}/${fileNames[j]}`);
     let png = PNG.sync.read(data);
 
-    let myImageData = {
-      width: 128,
-      height: 128,
+    let imageData = {
+      width: 224,
+      height: 224,
       data: png.data,
     };
 
@@ -73,17 +71,17 @@ module.exports = (customOption, save_dir) => {
     ];
     options.linefilter = true;
 
-    let svgstring = ImageTracer.imagedataToSVG(myImageData, options);
-    fs.writeFileSync(`./svg/${fileName[j]}.svg`, svgstring);
+    let svgString = ImageTracer.imagedataToSVG(imageData, options);
+    fs.writeFileSync(`svgs/${fileNames[j]}.svg`, svgString);
   }
 
   // 폰트 이벤트리스너
   fontStream
-    .pipe(fs.createWriteStream("./svg-font/font.svg"))
+    .pipe(fs.createWriteStream("svg_font/font.svg"))
     .on("finish", () => {
-      let svgdata = fs.readFileSync("./svg-font/font.svg", "utf8");
+      let svgdata = fs.readFileSync("svg_font/font.svg", "utf8");
       let ttf = svg2ttf(svgdata, {});
-      fs.writeFileSync("./ttf-fonts/customfont.ttf", new Buffer(ttf.buffer));
+      fs.writeFileSync("fonts/customfont.ttf", new Buffer(ttf.buffer));
     })
     .on("error", (err) => {
       console.log(err);
@@ -91,7 +89,7 @@ module.exports = (customOption, save_dir) => {
 
   // 여러 SVG 파일들을 하나의 SVG 파일로 생성
   for (let i = 0; i < sources.length; i++) {
-    let glyph = fs.createReadStream(`./svg/${fileName[i]}.svg`);
+    let glyph = fs.createReadStream(`svgs/${fileNames[i]}.svg`);
     glyph.metadata = {
       unicode: [String.fromCharCode(sources[i].toString(10))],
       name: `glyph${sources[i]}`,
